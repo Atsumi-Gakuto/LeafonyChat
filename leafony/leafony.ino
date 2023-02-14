19f6073e7ca663497e457a483211ed1c6b44cb86
Atsumi-Gakuto <k20006kk@aitech.ac.jp>
2023-02-14 15:30:18 +0900

    コネクションの確立と切断

--- leafony/leafony.ino
+++ leafony/leafony.ino
@@ -3,6 +3,7 @@
 #include <TBGLib.h>
 
 //** 定数 **
+//ピン番号
 #define BLE_INTERRUPT_PIN 2 //BLE割り込みピン
 #define BLE_RESET_PIN 6 //BLEリセットピン
 #define BLE_WAKEUP_PIN 7 //BLE起動ピン
@@ -16,33 +17,9 @@ const String Device_Name = "Leafony"; //Bluetoothに載せるデバイス名
 //BLE
 SoftwareSerial Serial_BLE(BLE_RX, BLE_TX);
 BGLib BLE((HardwareSerial *)&Serial_BLE, 0, 0);
+bool BLE_Connected = false; //BLEが接続されているかどうか。
 
 //** グローバル関数 **
-//アドバタイズデータの設定
-void start_advertise_data() {
-	uint8 advertising_data[31]; //アドバタイジングのデータ（最大31オクテット）
-	uint8 advertising_data_pointer = 0; //アドバタイジングのデータの格納場所を指し示す変数
-
-	//AD Structure 1（フラグ）
-	advertising_data[advertising_data_pointer++] = 0x02; //フィールドの長さ
-	advertising_data[advertising_data_pointer++] = BGLIB_GAP_AD_TYPE_FLAGS; //データタイプ
-	advertising_data[advertising_data_pointer++] = 0b110; //LE 一般検出可能モード、Bluetooth Classic 非対応
-
-	//AD Structure2（デバイス名）
-	advertising_data[advertising_data_pointer++] = Device_Name.length() + 1; //フィールドの長さ
-	advertising_data[advertising_data_pointer++] = BGLIB_GAP_AD_TYPE_LOCALNAME_COMPLETE;  //データタイプ
-	for(int i = 0; i < Device_Name.length(); i++) advertising_data[advertising_data_pointer++] = Device_Name.charAt(i); //デバイス名を1文字ずつ格納
-
-	//アドバタイズデータの登録
-	BLE.ble_cmd_le_gap_set_adv_data(SCAN_RSP_ADVERTISING_PACKETS, advertising_data_pointer, advertising_data);
-	while(BLE.checkActivity(1000)); //受信チェック
-
-	//アドバタイズ開始
-  Serial.println("Start advertising");
-	BLE.ble_cmd_le_gap_start_advertising(0, LE_GAP_USER_DATA, LE_GAP_SCANNABLE_NON_CONNECTABLE);
-	while(BLE.checkActivity(1000)); //受信チェック
-}
-
 void setup() {
   Serial.begin(9600); //シリアル通信の開始
   Serial.println("Setup start");
@@ -52,6 +29,19 @@ void setup() {
 	pinMode(BLE_RESET_PIN, OUTPUT);
 	pinMode(BLE_WAKEUP_PIN, OUTPUT);
 	digitalWrite(BLE_WAKEUP_PIN, HIGH);
+
+  //BLEライブラリのイベント関数
+  //機器が接続された時
+  BLE.ble_evt_le_connection_opend = []() {
+    Serial.println("Connection opened");
+    BLE_Connected = true;
+  };
+  //機器が切断された時
+  BLE.ble_evt_le_connection_closed = []() {
+    Serial.println("Connection closed");
+    BLE.ble_cmd_le_gap_start_advertising(0, LE_GAP_USER_DATA, LE_GAP_UNDIRECTED_CONNECTABLE);
+    BLE_Connected = false;
+  };
 
   //BLEのセットアップ
   Serial_BLE.begin(9600); //BLEシリアルの開始
@@ -63,12 +53,36 @@ void setup() {
     （詳しくは、pp.69-70参照）
   */
   while(BLE.checkActivity(1000)); //受信チェック
-  Serial.println("Setup end");
   BLE.ble_cmd_system_get_bt_address();
   while(BLE.checkActivity(1000)); //受信チェック
-  start_advertise_data();
+  Serial.println("Setup end");
+
+  //======================
+  //Advertisingデータの設定
+  uint8 advertising_data[31]; //Advertisingのデータ（最大31オクテット）
+	uint8 advertising_data_pointer = 0; //Advertisingのデータの格納場所を指し示す変数
+
+	//AD Structure 1（フラグ）
+	advertising_data[advertising_data_pointer++] = 0x02; //フィールドの長さ
+	advertising_data[advertising_data_pointer++] = BGLIB_GAP_AD_TYPE_FLAGS; //データタイプ
+	advertising_data[advertising_data_pointer++] = 0b110; //LE 一般検出可能モード、Bluetooth Classic 非対応
+
+	//AD Structure2（デバイス名）
+	advertising_data[advertising_data_pointer++] = Device_Name.length() + 1; //フィールドの長さ
+	advertising_data[advertising_data_pointer++] = BGLIB_GAP_AD_TYPE_LOCALNAME_COMPLETE;  //データタイプ
+	for(int i = 0; i < Device_Name.length(); i++) advertising_data[advertising_data_pointer++] = Device_Name.charAt(i); //デバイス名を1文字ずつ格納
+
+	//Advertisingデータの登録
+  BLE.ble_cmd_le_gap_set_adv_data(SCAN_RSP_ADVERTISING_PACKETS, advertising_data_pointer, advertising_data);
+	while(BLE.checkActivity(1000)); //受信チェック
+
+	//Advertising開始
+  Serial.println("Start advertising");
+  BLE.ble_cmd_le_gap_start_advertising(0, LE_GAP_USER_DATA, LE_GAP_UNDIRECTED_CONNECTABLE); //接続可・非指向性
+	while(BLE.checkActivity(1000)); //受信チェック
 }
 
 void loop() {
   if(Serial.available() > 0) Serial.write(Serial.read());
+  BLE.checkActivity(0); //BLEの状態の確認
 }
